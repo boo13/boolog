25223bfa3db148a1e16fdafdc0fe9329905dc0ea
boo 13 <boo13bot@gmail.com>
2019-11-01 00:50:51 -0400

    following along with Michael Kennedy's tutorial series from Talk Python

--- app.py
+++ app.py
@@ -5,30 +5,23 @@ folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
 
 
-from boolog.infrastructure.view_modifiers import response
-
 app = flask.Flask(__name__)
 
 
-def get_latest_blog_posts():
-    return [
-        {"id": 0, "name": "test1", "date": "friday"},
-        {"id": 1, "name": "test2", "date": "tuesday"},
-    ]
-
-
-@app.route("/")
-@response(template_file='home/index.html')
-def index():
-    _blog = get_latest_blog_posts()
-    return {'posts': _blog}
-
-
-@app.route("/about")
-@response(template_file='home/about.html')
-def about():
-    return {}
-
-
-if __name__ == "__main__":
+def main():
+    register_blueprints()
     app.run(debug=True)
+
+
+def register_blueprints():
+    from boolog.views import home_views
+    from boolog.views import package_views
+    from boolog.views import cms_views
+
+    app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(home_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
+
+
+if __name__ == '__main__':
+    main()
