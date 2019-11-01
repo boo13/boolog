a5d074026d7cbac29dbcc8fa7090663707a802cc
boo 13 <boo13bot@gmail.com>
2019-11-01 00:50:39 -0400

    following along with Michael Kennedy's tutorial series from Talk Python

--- infrastructure/view_modifiers.py
+++ infrastructure/view_modifiers.py
@@ -6,10 +6,6 @@ import werkzeug.wrappers
 
 
 def response(*, mimetype: str = None, template_file: str = None):
-    '''
-    Code thanks to: Michael Kennedy @ Talk Python
-    https://github.com/talkpython/data-driven-web-apps-with-flask/tree/master/app/ch05_jinja_templates/final/pypi_org
-    '''
     def response_inner(f):
         # print("Wrapping in response {}".format(f.__name__), flush=True)
 
@@ -45,3 +41,19 @@ def response(*, mimetype: str = None, template_file: str = None):
         return view_method
 
     return response_inner
+
+#
+# def template(template_file: str = None):
+#     def template_inner(f):
+#         @wraps(f)
+#         def view_method(*args, **kwargs):
+#             data_dict = f(*args, **kwargs)
+#             if not isinstance(data_dict, dict):
+#                 raise Exception(
+#                     "Invalid return type {}, we expected a dict as the return value.".format(type(data_dict)))
+#
+#             return flask.render_template(template_file, **data_dict)
+#
+#         return view_method
+#
+#     return template_inner
